8d5947bc569336c424a21f98b33446baead93651
Ajmal <ajmalazeez007@gmail.com>
2017-04-29 21:55:01 +0530

    complete project

---
+++ app/src/main/java/com/example/xy/wishlist/BrowseWishFragment.java
@@ -0,0 +1,29 @@
+package com.example.xy.wishlist;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BrowseWishFragment extends Fragment {
+
+
+    public BrowseWishFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_browse_wish, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/xy/wishlist/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.xy.wishlist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+
+    }
+
+}
